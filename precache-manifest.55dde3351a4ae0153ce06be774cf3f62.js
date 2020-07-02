2018ed1c608f8e16e9b1da9366cc6fec040cde94
Van Haddad <haddadvan@hotmail.com>
2020-07-02 13:16:55 -0400

    Updates

---
+++ precache-manifest.55dde3351a4ae0153ce06be774cf3f62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cc2ee179dc432d641a87121173e281",
+    "url": "/react-clicker-game/index.html"
+  },
+  {
+    "revision": "f0db28a7970b76e00f97",
+    "url": "/react-clicker-game/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "38dd3643c640df2a8e58",
+    "url": "/react-clicker-game/static/css/main.e8d83661.chunk.css"
+  },
+  {
+    "revision": "f0db28a7970b76e00f97",
+    "url": "/react-clicker-game/static/js/2.d97cafc6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-clicker-game/static/js/2.d97cafc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38dd3643c640df2a8e58",
+    "url": "/react-clicker-game/static/js/main.0ce34e7c.chunk.js"
+  },
+  {
+    "revision": "34b61a9a13b2c387d2a1",
+    "url": "/react-clicker-game/static/js/runtime-main.32186e24.js"
+  }
+]);
